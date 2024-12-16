e40483c08471e693277aa825172f26188c3bca59
liamfking518 <32520782+liamfking518@users.noreply.github.com>
2024-12-16 13:43:20 -0500

    add friendly greeting message

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
+  const [count, setCount] = useState(0)
 
-        <h1>Hello from Amplify</h1>
-      </header>
-    </div>
-  );
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Hello Mike and Jory! Hope you guys have a great holiday season!!
+        </p>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
